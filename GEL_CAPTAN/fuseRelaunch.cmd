79adb1e612f2ab47b3f3a3b657038c7c51131aee
CollinBradford <cbsoftwareandengineering@gmail.com>
2017-03-15 11:40:54 -0500

    Just deleting stuff so that I can sync everything

--- GEL_CAPTAN/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "secureip" -o "D:/cbradford/NewADCFirmware/GEL_CAPTAN/ClockLatchSignals_tb_isim_beh.exe" -prj "D:/cbradford/NewADCFirmware/GEL_CAPTAN/ClockLatchSignals_tb_beh.prj" "work.ClockLatchSignals_tb" 
